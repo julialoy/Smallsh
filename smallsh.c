642410752ec586146acec9e23edfa0ead487147b
Julia Loy <julialoy@gmail.com>
2023-02-06 21:54:38 -0500

    Added functionality to check for user or root and print appropriate prompt.

--- smallsh.c
+++ smallsh.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -5,8 +6,10 @@
 #include <string.h>
 #include <sys/types.h> /* For pid_t type */
 #include <sys/wait.h>
+#include <signal.h>
 #include <errno.h>
 #include <err.h>
+
 
 // Main function takes no arguments:
 // User will supply input in response to the command prompt
@@ -17,33 +20,41 @@ int main(void) {
   // Infinite loop to perform the shell functionality
   for (size_t i = 0; i < 5; ++i) {
     // Check for any unwaited-for background processes in the same process group ID as this program
-    // Get smallsh's PGID
-    // pid_t curr_pgid;
-    // curr_pgid = getpgrp();
-    // Error check getpgrp not necessary, always successfully returns PGID
-    // if (errno != 0) err(errno, "Unable to get group ID for current process");
-    // Get all processes with curr_pgid as PGID and print correct message for each
-    // Pass 0 as PID to waitpid to request status for any child process with same process group ID as smallsh
-    // int *child_proc_status = NULL;
-    // pid_t child_proc_pid = waitpid(0, child_proc_status, WNOHANG);
-    // Error check waitpid
-    for (size_t j = 0; j < 5; ++j) {
-      int child_proc_status;
-      pid_t child_proc_pid = waitpid(0, &child_proc_status, WNOHANG);
-      
-      // Handle error case and case where there are no background processes in smallsh's process group
-      if (child_proc_pid == -1) {
-        err(errno, "An error occurred in waitpid");
-      } else if (child_proc_pid == 0) {
-        break;
+    int child_proc_status;
+    pid_t child_proc_pid;
+
+    // Loop through all child processes that have the same process group ID
+    // Passing 0 to waitpid as the pid means waitpid will check all children with the current process's process group ID
+    // Print correct info message to stderr for all child processes found
+    while ((child_proc_pid = waitpid(0, &child_proc_status, WUNTRACED | WNOHANG | WCONTINUED)) > 0) {
+      // Use of macros from CS344 modules and Linux Programming Interface text
+      if (WIFEXITED(child_proc_status)) {
+        if (fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t) child_proc_pid, WEXITSTATUS(child_proc_status)) < 0) err(errno, "Error occurred in fprintf");
+      } else if (WIFSIGNALED(child_proc_status)) {
+        if (fprintf(stderr, "Child process %jd done. Signaled %d.\n", (intmax_t) child_proc_pid, WTERMSIG(child_proc_status)) < 0) err(errno, "Error occurred in fprintf");
+      } else if (WIFSTOPPED(child_proc_status)) {
+        if (kill(child_proc_pid, SIGCONT) == -1) err(errno, "Unable to send signal");
+        if (fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) child_proc_pid) < 0) err(errno, "Error occurred in fprintf");
       }
-
-      // Change this to the correct message after testing
-      fprintf(stderr, "Child process %jd has status %d", (intmax_t)child_proc_pid, child_proc_status);
-
+    }
+    // Checking errno taken from Linux Programming Interface wait example, chap. 26
+    if (errno != ECHILD) err(errno, "Error occurred in waitpid");
+    // Reset errno 
+    errno = 0;
+    
+    // Print interactive input prompt for user
+    // Determine if user or root
+    char const *restrict env_name_1 = "USER";
+    char const *restrict env_name_2 = "LOGNAME";
+    // char const *restrict root_name = "root";
+    // char prompt = '$';
+    if (strcmp(getenv(env_name_1), "root") == 0 || strcmp(getenv(env_name_2), "root") == 0) {
+      // prompt = '#';
+      if (fprintf(stderr, "#") < 0) err(errno, "Error occurred in fprintf");
+    } else {
+      if (fprintf(stderr, "$") < 0 ) err(errno, "Error occurred in fprintf");
     }
     goto exit;
-    // Print interactive input prompt for user
   }
 
 /* TODO: Parse command line input into semantic tokens */
