7277085d8927644ee4320248938730250830d630
Julia Loy <julialoy@gmail.com>
2023-02-07 13:15:44 -0500

    Moved functionality to check waiting background processes into function. Added testing statements to help see what code is doing.

--- smallsh.c
+++ smallsh.c
@@ -10,6 +10,37 @@
 #include <errno.h>
 #include <err.h>
 
+// Declare function check_back_procs
+// Function handles any unwaited for background processes
+int check_back_procs(void) {
+  int child_proc_status;
+  pid_t child_proc_pid;
+  
+  // TESTING STATEMENT
+  fprintf(stderr, "Checking background processes . . .\n");
+
+  while ((child_proc_pid = waitpid(0, &child_proc_status, WUNTRACED | WNOHANG | WCONTINUED)) > 0) {
+    if (WIFEXITED(child_proc_status)) {
+      if (fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t) child_proc_pid, WEXITSTATUS(child_proc_status)) < 0) goto exit;
+    } else if (WIFSIGNALED(child_proc_status)) {
+      if (fprintf(stderr, "Child process %jd done. Signaled %d.\n", (intmax_t) child_proc_pid, WTERMSIG(child_proc_status)) < 0) goto exit; 
+    } else if (WIFSTOPPED(child_proc_status)) {
+      if (kill(child_proc_pid, SIGCONT == -1)) goto exit;
+      if (fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) child_proc_pid) < 0) goto exit;
+    }
+  }
+
+  if (errno != ECHILD && errno != 0) goto exit;
+  
+  // TESTING STATEMENT
+  if (errno == ECHILD) fprintf(stderr, "No additional background processes found.\n");
+  
+  // Explicitly reset errno
+  errno = 0;
+// Return statement taken from skeleton code for tree assignment
+exit:
+  return errno ? -1 : 0;
+}
 
 // Main function takes no arguments:
 // User will supply input in response to the command prompt
@@ -19,8 +50,8 @@ int main(void) {
   char const *restrict env_name_2 = "LOGNAME";
   char *line = NULL;
   size_t n = 0;
-  int child_proc_status;
-  pid_t child_proc_pid;
+  //int child_proc_status;
+  //pid_t child_proc_pid;
   // Explicitly set errno prior to doing anything
   errno = 0;
 
@@ -32,7 +63,8 @@ int main(void) {
     // Loop through all child processes that have the same process group ID
     // Passing 0 to waitpid as the pid means waitpid will check all children with the current process's process group ID
     // Print correct info message to stderr for all child processes found
-    while ((child_proc_pid = waitpid(0, &child_proc_status, WUNTRACED | WNOHANG | WCONTINUED)) > 0) {
+    if (check_back_procs() != 0) err(errno, "Error occurred while checking background processes");
+    /* while ((child_proc_pid = waitpid(0, &child_proc_status, WUNTRACED | WNOHANG | WCONTINUED)) > 0) {
       // Use of macros from CS344 modules and Linux Programming Interface text
       if (WIFEXITED(child_proc_status)) {
         if (fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t) child_proc_pid, WEXITSTATUS(child_proc_status)) < 0) err(errno, "Error occurred in fprintf");
@@ -42,17 +74,14 @@ int main(void) {
         if (kill(child_proc_pid, SIGCONT) == -1) err(errno, "Unable to send signal");
         if (fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) child_proc_pid) < 0) err(errno, "Error occurred in fprintf");
       }
-    }
+    }*/
 
     // Checking errno taken from Linux Programming Interface wait example, chap. 26
-    if (errno != ECHILD) err(errno, "Error occurred in waitpid");
+    /* if (errno != ECHILD) err(errno, "Error occurred in waitpid"); */
     // Reset errno 
-    errno = 0;
+   // errno = 0;
     
-    // Print interactive input prompt for user
-    // Determine if user or root
-    // char const *restrict root_name = "root";
-    // char prompt = '$';
+    // Determine if user or root and print prompt for user
     if (strcmp(getenv(env_name_1), "root") == 0 || strcmp(getenv(env_name_2), "root") == 0) {
       // prompt = '#';
       if (fprintf(stderr, "#") < 0) err(errno, "Error occurred in fprintf");
@@ -60,11 +89,21 @@ int main(void) {
       if (fprintf(stderr, "$") < 0 ) err(errno, "Error occurred in fprintf");
     }
     
-    // Loop to get input
+    // Read a line of input
     // Code taken from example in smallsh assignment specs
     ssize_t line_length = getline(&line, &n, stdin);
     if (line_length == -1 && errno != EOF) err(errno, "Error occurred in getline");
     if (fprintf(stderr, "Line entered was: %s", line) < 0) err(errno, "Error occurred in fprintf");
+    
+    // Clear EOF and error indicators for stdin
+    if (feof(stdin) != 0 || ferror(stdin) != 0) {
+      // Per man pages this function should not fail
+      if (fprintf(stderr, "Clearing EOF and errors") < 0) err(errno, "Error occurred in fprintf"); /* DEBUGGING */
+      clearerr(stdin);
+    }
+    // Reset errno
+    errno = 0;
+    // SIGNAL HANDLING IMPLEMENTED HERE
     goto exit;
   }
 
@@ -81,3 +120,5 @@ exit:
   free(line);
   return 0;
 }
+
+
