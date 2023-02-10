600094b3f00b1a36874e63390e7db9cca7d59ae1
Julia Loy <julialoy@gmail.com>
2023-02-09 23:00:01 -0500

    Expansion functionality has bugs when searching for substring now. Errors when a command with arguments is given.

--- smallsh.c
+++ smallsh.c
@@ -19,7 +19,7 @@ char *str_gsub(char *restrict *restrict orig_str, char const *restrict pattern, 
   size_t orig_str_len = strlen(expanded_str);
   size_t const pattern_len = strlen(pattern), sub_len = strlen(sub);
   
-  if (fprintf(stderr, "Searching for %s in %s, replacing with %s\n", pattern, *orig_str, sub) < 0) goto str_gsub_return;
+ //  if (fprintf(stderr, "Searching for %s in %s, replacing with %s\n", pattern, *orig_str, sub) < 0) goto str_gsub_return;
   for (; (expanded_str = strstr(expanded_str, pattern));) {
     ptrdiff_t offset = expanded_str - *orig_str;
     if (sub_len > pattern_len) {
@@ -38,11 +38,14 @@ char *str_gsub(char *restrict *restrict orig_str, char const *restrict pattern, 
   expanded_str = *orig_str;
   if (sub_len < pattern_len) {
     expanded_str = realloc(*orig_str, sizeof **orig_str * (orig_str_len + 1));
-    if (!expanded_str) goto str_gsub_return;
+    if (!expanded_str) {
+      expanded_str = *orig_str;
+      goto str_gsub_return;
+    }
     *orig_str = expanded_str;
   }
 
-  fprintf(stderr, "Final string is %s\n", expanded_str);
+  // fprintf(stderr, "Final string is %s\n", expanded_str);
 
 str_gsub_return:
   return expanded_str;
@@ -62,13 +65,13 @@ int main(void) {
   char const *restrict ifs_var_name = "IFS";
   char **word_tokens = NULL; /* Initially set to NULL; per assignment specs, feeling fancy */
   int last_fg_exit_status = 0; /* To hold the exit status of the last foreground process. Default is 0. */
-  char last_bg_proc_pid_char; /* To hold the PID of the most recent background process. Cast to char type for str search. Default is empty str. */
+  int last_bg_proc_pid = -1; /* To hold the PID of the most recent background process. Cast to char type for str search. Default is empty str. */
   size_t max_len_buff = 20; /*To be used with snprintf for changing number types to strings */
   // Explicitly set errno prior to doing anything
   errno = 0;
 
   // Infinite loop to perform the shell functionality
-  for (size_t i = 0; i < 5; ++i) {
+  for (;;) {
     // 1a. Manage background processes
     // Check for any unwaited-for background processes in the same process group ID as this program
     // Loop through all child processes that have the same process group ID
@@ -105,13 +108,15 @@ int main(void) {
     // Code taken from example in smallsh assignment specs
     ssize_t line_length = getline(&line, &n, stdin);
     if (line_length == -1 && errno != EOF) goto exit;
-    if (fprintf(stderr, "Line entered was: %s", line) < 0) goto exit;
+    // if (fprintf(stderr, "Line entered was: %s", line) < 0) goto exit;
     
     // Clear EOF and error indicators for stdin
-    if (feof(stdin) != 0 || ferror(stdin) != 0) {
+    if (feof(stdin) != 0) {
       // Per man pages this function should not fail
-      if (fprintf(stderr, "Clearing EOF and errors") < 0) goto exit; /* DEBUGGING */
+      //if (fprintf(stderr, "Clearing EOF and errors") < 0) goto exit;*/ /* DEBUGGING */ 
       clearerr(stdin);
+      line = "exit $?";
+      line_length = strlen(line);
     }
     // Reset errno in case EOF was encountered when reading from stdin
     errno = 0;
@@ -131,7 +136,7 @@ int main(void) {
     char *token = strtok(line, word_delim);
     char *dup_token;
     
-    if (!token) continue; /* If there were no words to split, go back to beginning of loop and display prompt */
+    if (!token) continue; /* If there were no words to split, go back to beginning of loop and display prompt. Num_tokens == 0 at this point. Do not reset. */
     dup_token = strdup(token);
     if (!dup_token) goto exit; /* Error checks strdup */
       
@@ -154,6 +159,10 @@ int main(void) {
       word_tokens[num_tokens] = dup_token; /* Add pointer to new_token to word_tokens */
       ++num_tokens;
     }
+
+    // Explicitly add NULL pointer to end of word_tokens to allow for use as arg to execvp later in shell program
+    word_tokens = realloc(word_tokens, sizeof (char **[num_tokens+1]));
+    word_tokens[num_tokens+1] = NULL;
    
     // 2. Expansion
     // String search adapted from instructor's video linked in CS344's smallsh assignment specs
@@ -176,11 +185,14 @@ int main(void) {
       
       // Expand $$ instances
       target_pattern = "$$";
-      fprintf(stderr, "Shell PID is %jd\n", (intmax_t) shell_pid);
-      char proc_pid_as_str[max_len_buff];
-      if (snprintf(proc_pid_as_str, max_len_buff-1, "%jd", (intmax_t) shell_pid) < 0) goto exit;
+//      fprintf(stderr, "Shell PID is %jd\n", (intmax_t) shell_pid);
+     char proc_pid_as_str[max_len_buff];
+     memset(proc_pid_as_str, '\0', max_len_buff);
+     
+     if (snprintf(proc_pid_as_str, max_len_buff-1, "%jd", (intmax_t) shell_pid) < 0) goto exit;
+     //if (sprintf(proc_pid_as_str, "%jd", (intmax_t) shell_pid) < 0) goto exit;
 
-      fprintf(stderr, "Shell PID as string is %s\n", proc_pid_as_str);
+//      fprintf(stderr, "Shell PID as string is %s\n", proc_pid_as_str);
       substitution = proc_pid_as_str;
       word_tokens[i] = str_gsub(&word_tokens[i], target_pattern, substitution);
       if (errno != 0) goto exit;
@@ -196,29 +208,117 @@ int main(void) {
         last_exit_status = WIFEXITED(last_exit_status);
       } */
       char exit_status_as_str[max_len_buff];
+      memset(exit_status_as_str, '\0', max_len_buff);
+      //char *exit_status_as_str = NULL;
       if (snprintf(exit_status_as_str, max_len_buff-1, "%d", last_fg_exit_status) < 0) goto exit;
+      // if (sprintf(exit_status_as_str, "%d", last_fg_exit_status) < 0) goto exit;
       substitution = exit_status_as_str;
-      fprintf(stderr, "Last exit status is %s\n", substitution);
+  //    fprintf(stderr, "Last exit status is %s\n", substitution);
       word_tokens[i] = str_gsub(&word_tokens[i], target_pattern, substitution);
       if (errno != 0) goto exit;
 
       // Expande $! instances
       target_pattern = "$!";
-      if (!last_bg_proc_pid_char) {
-        substitution = &last_bg_proc_pid_char;
-      } else {
+      if (last_bg_proc_pid == -1) {
         substitution = "";
+      } else {
+        char last_bg_proc_pid_str[max_len_buff];
+        memset(last_bg_proc_pid_str, '\0', max_len_buff);
+        // char *last_bg_proc_pid_str = NULL;
+        if (snprintf(last_bg_proc_pid_str, max_len_buff-1, "%d", last_bg_proc_pid) < 0) goto exit;
+        // if (sprintf(last_bg_proc_pid_str, "%d", last_bg_proc_pid) < 0) goto exit;
+        substitution = last_bg_proc_pid_str;
       }
       word_tokens[i] = str_gsub(&word_tokens[i], target_pattern, substitution);
       if (errno != 0) goto exit;
     }
     
     // DEBUGGING/TESTING 
-    for (size_t j = 0; j < num_tokens; ++j) {
+    /*for (size_t j = 0; j < num_tokens; ++j) {
       fprintf(stderr, "Expanded token is %s\n", word_tokens[j]);
-    }
+    }*/
+    // 4. Parsing: TO COME
 
-    goto exit;
+    // 5. Execution
+    
+    // Branch for no command word entered in stdin
+
+    // Branch for built-in command exit
+    // First word/token is command
+    if (strcmp(word_tokens[0], "exit") == 0) {
+      if (num_tokens > 2) {
+        errno = -1;
+        fprintf(stderr, "Too many arguments passed to exit command");
+        goto exit;
+      }
+      
+      // MISSING: SEND ALL CHILD PROCESSES SIGINT BEFORE EXITING
+      int shell_exit_status = last_fg_exit_status;
+      // No argument provided
+      if (num_tokens == 1) {
+        fprintf(stderr, "\nexit\n");
+        exit(shell_exit_status);
+      } else {
+        // MISSING: ERROR IF ARG TO EXIT IS NOT AN INT
+        fprintf(stderr, "\nexit\n");
+        shell_exit_status = atoi(word_tokens[1]);
+        exit(shell_exit_status);
+      }
+    } else if (strcmp(word_tokens[0], "cd") == 0) { /* Branch for built-in command cd */
+      if (num_tokens > 2) {
+        errno = -1;
+        fprintf(stderr, "Too many arguments passed to cd command");
+        goto exit;
+      }
+
+      char *cd_arg = getenv("HOME");
+      if (num_tokens == 2) {
+        cd_arg = word_tokens[1];
+      }
+      if (chdir(cd_arg) != 0) goto exit;
+
+    } else { /* Branch for non-built-in commands */
+      // Execute non-built-in-command in a new child process
+      // execvp will search PATH variable for command if command does not start with slash
+      // Code below adapted from exampole code in CS344 module Process API - Executing a New Program
+      int new_child_status;
+      pid_t new_child_pid = fork();
+
+      switch(new_child_pid) {
+        case -1:
+          goto exit; /* Error checks fork */
+          break;
+        case 0: /* Process is child */
+          // MISSING: Handling output redirection operator and input redirection opperator
+          if (execvp(word_tokens[0], word_tokens) == -1) {
+            if (fprintf(stderr, "An error occurred while trying to run command %s\n", word_tokens[0]) < 0) goto exit;
+            goto exit;
+            // break;
+          }
+          break;
+        default: /* Process is parent */
+          if (waitpid(new_child_pid, &new_child_status, 0) == -1) goto exit; /* Blocking wait for child process with error checking */
+          if (WIFSIGNALED(new_child_status)) {
+            last_fg_exit_status = 128 + WTERMSIG(new_child_status);
+          } else if (WIFSTOPPED(new_child_status)) {
+            if (fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) new_child_pid) < 0) goto exit;
+            kill(new_child_pid, SIGCONT);
+            last_bg_proc_pid = new_child_pid;
+          } else if (WIFEXITED(new_child_status)) {
+            last_fg_exit_status = new_child_status; /* Set shell variable $? to exit status of this command */
+          }
+          break;
+      }
+
+    }
+   // goto exit;
+   // Free things!
+   free(line);
+   for (size_t x = 0; x < num_tokens; ++x) {
+     free(word_tokens[x]);
+   }
+   free(word_tokens);
+   num_tokens = 0;
   }
 
 /* TODO: Parse command line input into semantic tokens */
@@ -233,11 +333,15 @@ int main(void) {
   // Based on CS344 tree assignment skeleton code
 exit:
   // Free line pointer used for input
-  free(line);
-  for (size_t x = 0; x < num_tokens; ++x) {
-    free(word_tokens[x]);
+  if (errno != 0) {
+    if (line != NULL) free(line);
+    if (num_tokens > 0 && word_tokens != NULL) {
+      for (size_t x = 0; x < num_tokens; ++x) {
+        free(word_tokens[x]);
+      }
+      free(word_tokens);
+    }
   }
-  free(word_tokens);
   return errno ? -1 : 0;
 }
 
